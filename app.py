b5eb033f351861d81d64beec2f8c59dc78840993
MohamadEdris <73100170+MohamadEdris@users.noreply.github.com>
2022-07-17 11:33:08 +0300

    Upload updated files

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import bitCoin
+import redis
+
+app = Flask(__name__)
+btcData = redis.Redis(host='redis', port=6379)
+
+
+@app.route("/")
+
+def home():
+    # True loop used for getting the price in less than minute
+    while True:
+
+        price = float("{:.2f}".format(bitCoin.gitCurBTCPrice()))
+        avgPrice = float("{:.2f}".format(bitCoin.gitAvgBtCPrice()))
+
+        # save it in btcData redis file
+        btcData.set('CurrPrice', price)
+        btcData.set('AvgPrice', avgPrice)
+
+        #refresh to update the content in page without clicking refresh each time
+        return """
+        <meta http-equiv="refresh" content="1" /><h2>Docker Final Task, Current BTC Price</h2><br> <h3>Current BitCoin Price is: {}$</h3><br> <h3>Average BitCoin Price Last 10 Minutes: {}$ </h3><br> """.format(price,avgPrice)
---
+++ bitCoin.py
@@ -0,0 +1,19 @@
+import requests
+
+# Class to git BTC current Price
+# Calculate the avg of bitcion price for the last ten minutes
+
+# return average of bitcoin price for the last 10 minutes
+def gitAvgBtCPrice():
+    # save in the variable objects in json format
+    sum=0
+    BTCpriceMinute = requests.get('https://min-api.cryptocompare.com/data/v2/histominute?fsym=BTC&tsym=USD&limit=9').json()['Data']['Data']
+    for bt in BTCpriceMinute:
+        sum+=bt['close']
+    return (sum/10)
+
+# return current bitcoin price function
+def gitCurBTCPrice(): 
+    # this request.get returns in json format a single object and the ['USD'] get the value(price) of the current bitcion price.
+    CurrentBtc = requests.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD').json()['USD']
+    return CurrentBtc
